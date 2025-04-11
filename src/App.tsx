11adedcc0a492c7a61133ce39b06db32c074638b
Andrii Fesych <andriyhimself@gmail.com>
2025-04-11 20:31:39 +0200

    _

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import { Navigate, Route, Routes } from 'react-router-dom';
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { HomePage } from './components/HomePage';
+import { Navbar } from './components/Navbar';
+import { NotFoundPage } from './components/NotFound';
+import { LocalTodos } from './components/LocalTodos';
+import { CloudTodos } from './components/CloudTodos';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div data-cy="app">
+      <Navbar />
+
+      <main className="section">
+        <div className="container">
+          <div className="block">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/local" element={<LocalTodos />} />
+              <Route path="/local/active" element={<LocalTodos />} />
+              <Route path="/local/completed" element={<LocalTodos />} />
+              <Route path="/cloud" element={<CloudTodos />} />
+              <Route path="/cloud/active" element={<CloudTodos />} />
+              <Route path="/cloud/completed" element={<CloudTodos />} />
+              <Route path="/home" element={<Navigate to="/" replace />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,15 @@
 import { createRoot } from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';
+
+import './styles/index.scss';
+import './styles/todo-list.css';
+import './styles/filters.css';
+
 import { App } from './App';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+createRoot(document.getElementById('root') as HTMLDivElement)
+  .render(
+    <Router>
+      <App />
+    </Router>,
+  );
